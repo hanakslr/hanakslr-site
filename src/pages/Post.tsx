7bc18a8d8bb28b714d7fb15de83032bb71dc931b
Hana Kessler <hanakessler18@gmail.com>
2025-03-02 11:15:35 -0500

    wip

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,3 +1,4 @@
+import { IconChevronLeft } from "@tabler/icons-react";
 import { Link, useLoaderData, useParams } from "@tanstack/react-router";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
@@ -9,16 +10,24 @@ export default function Post() {
   if (post.notFound) return <p>Post not found</p>;
 
   return (
-    <div>
-      <Link to="/" className="underline">
-        Return Home
+    <div className="flex flex-col gap-4 p-2 sm:p-4">
+      <Link
+        to="/"
+        className="border-bottom flex flex-row items-center gap-1 hover:text-yellow-400"
+      >
+        <IconChevronLeft size={16} /> <div>Return Home</div>
       </Link>
-      <article className="prose">
-        <h1>{slug.replace(/-/g, " ")}</h1>
-        <ReactMarkdown remarkPlugins={[remarkGfm]}>
-          {post.content}
-        </ReactMarkdown>
-      </article>
+
+      <div className="flex w-full flex-col items-center gap-4 rounded-xl border-yellow-400 p-2 py-4 sm:border-2 md:gap-8 md:p-6 md:py-8">
+        <div className="flex w-2/3 border">
+          <article className="prose">
+            <h1>{slug.replace(/-/g, " ")}</h1>
+            <ReactMarkdown remarkPlugins={[remarkGfm]}>
+              {post.content}
+            </ReactMarkdown>
+          </article>
+        </div>
+      </div>
     </div>
   );
 }
