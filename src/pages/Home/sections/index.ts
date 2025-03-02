f272097f83922e57ea12c39063baa2ca19ba9640
Hana Kessler <hanakessler18@gmail.com>
2025-03-02 11:41:02 -0500

    placeholder for posts

--- src/pages/Home/sections/index.ts
+++ src/pages/Home/sections/index.ts
@@ -1,4 +1,5 @@
 import { Header } from "./Header";
 import { Github } from "./Github";
+import { Posts } from "./Posts";
 
-export { Header, Github as GithubLatest };
+export { Header, Github as GithubLatest, Posts };
