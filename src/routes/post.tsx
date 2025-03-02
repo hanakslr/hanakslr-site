915c35c6e0989a83f90e6113ec8c16753d54f696
Hana Kessler <58595147+hanakslr@users.noreply.github.com>
2025-03-02 15:51:57 -0500

    firebase hosting and gh actions deploys (#1)
    * setup up firebase hosting and actions deploys
    
    * need routeTree in src
    
    * dependencies error

--- src/routes/post.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/post")({
-  component: Post,
-});
-
-function Post() {
-  return <div>This is a post</div>;
-}
