4d1416f98f5c3f2c79ce4b8bc5ce4c1fd27cf219
Hana Kessler <hanakessler18@gmail.com>
2025-03-02 11:35:09 -0500

    add frontmatter for post metadata

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,10 +1,10 @@
 import { IconChevronLeft } from "@tabler/icons-react";
-import { Link, useLoaderData, useParams } from "@tanstack/react-router";
+import { Link, useLoaderData } from "@tanstack/react-router";
 import ReactMarkdown from "react-markdown";
+import remarkFrontmatter from "remark-frontmatter";
 import remarkGfm from "remark-gfm";
 
 export default function Post() {
-  const { slug } = useParams({ from: "/posts/$slug" });
   const post = useLoaderData({ from: "/posts/$slug" });
 
   if (post.notFound) return <p>Post not found</p>;
@@ -15,14 +15,13 @@ export default function Post() {
         to="/"
         className="border-bottom flex flex-row items-center gap-1 hover:text-yellow-400"
       >
-        <IconChevronLeft size={16} /> <div>Return Home</div>
+        <IconChevronLeft size={16} /> <div>Home</div>
       </Link>
 
-      <div className="flex w-full flex-col items-center gap-4 rounded-xl border-yellow-400 p-2 py-4 sm:border-2 md:gap-8 md:p-6 md:py-8">
-        <div className="flex w-2/3 border">
+      <div className="flex w-full flex-col items-center gap-4 rounded-xl p-2 py-4 md:gap-8 md:p-6 md:py-8">
+        <div className="flex">
           <article className="prose">
-            <h1>{slug.replace(/-/g, " ")}</h1>
-            <ReactMarkdown remarkPlugins={[remarkGfm]}>
+            <ReactMarkdown remarkPlugins={[remarkGfm, remarkFrontmatter]}>
               {post.content}
             </ReactMarkdown>
           </article>
