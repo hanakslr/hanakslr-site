7bb9bda5373aa3c3e17adfa478a40239de4157b7
Hana Kessler <hanakessler18@gmail.com>
2025-03-17 07:26:07 -0400

    2 import methods

--- src/pages/Wasm/index.tsx
+++ src/pages/Wasm/index.tsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+import * as wasm2 from "./pkg/spirograph_wasm";
 
 const loadWasm = async (name: string) => {
   // Fetch and instantiate the WASM module
-  const wasm = await import(`./${name}/strava_stats.js`);
+  const wasm = await import(`./${name}/spirograph_wasm.js`);
   await wasm.default();
 
   return wasm;
@@ -13,11 +14,21 @@ export const WasmPage = () => {
 
   useEffect(() => {
     const greetWasm = async () => {
-      const wasm = await loadWasm("pkg_1");
+      const wasm = await loadWasm("pkg");
       setMessage(wasm.greet());
     };
 
     greetWasm();
   });
-  return <div>{message}</div>;
+
+  const anotherMessage = useMemo(async () => {
+    return await wasm2.greet();
+  }, [wasm2]);
+
+  return (
+    <div>
+      <div> Message from import method 1: {message}</div>
+      <div>Message from import method 2: {anotherMessage}</div>
+    </div>
+  );
 };
