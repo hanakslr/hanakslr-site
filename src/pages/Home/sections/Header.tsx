01bd0fdd78df11d52132ab452c679fd5bfd4bb56
Hana Kessler <hanakessler18@gmail.com>
2025-07-14 13:20:41 -0400

    Update Header.tsx

--- src/pages/Home/sections/Header.tsx
+++ src/pages/Home/sections/Header.tsx
@@ -13,21 +13,32 @@ export const Header = () => {
   return (
     <WindowHeightSection>
       <div className="flex h-full w-full flex-col items-center justify-center rounded-xl border-2 border-blue-200">
-        <h1 className="w-full py-2 text-center text-2xl font-bold">
-          Hi, I'm Hana
-        </h1>
-        <p className="text-md font-thin">I'm a software engineer.</p>
-        <div className="flex flex-row gap-4 py-3">
-          {PROFILES.map((p) => (
-            <a
-              href={p.url}
-              target="_blank"
-              rel="noopener noreferrer"
-              id={p.name}
-            >
-              <p.icon className="h-6 w-6" />
-            </a>
-          ))}
+        <div className="w-full max-w-lg gap-4">
+          <div className="flex w-full flex-row justify-between">
+            <h1 className="w-full py-2 text-2xl font-bold">Hi, I'm Hana</h1>
+            <div className="mr-8 flex flex-row gap-4 py-3">
+              {PROFILES.map((p) => (
+                <a
+                  href={p.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  id={p.name}
+                >
+                  <p.icon className="h-6 w-6" />
+                </a>
+              ))}
+            </div>
+          </div>
+          <div className="text-md flex flex-col gap-2 font-thin">
+            <p>
+              I'm a software engineer. I specialize in backend systems that work
+              because they’re grounded in how people really use them.
+            </p>
+            <p>
+              When I'm not coding you can find me mountain biking, skate skiing,
+              or thinking about public transit.
+            </p>
+          </div>
         </div>
       </div>
     </WindowHeightSection>
